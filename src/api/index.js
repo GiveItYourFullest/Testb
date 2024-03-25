f066e93e61e63596e034af22adf5a3e35abc8baa
GiveItYourFullest <163412508+GiveItYourFullest@users.noreply.github.com>
2024-03-25 13:01:16 +0000

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,7 @@ const APIurl = 'https://jsonplaceholder.typicode.com/posts'
 
 export default async function fetchFaqs() {
   const response = await axios.get(APIurl)
+
+  
   return response.data
 }
